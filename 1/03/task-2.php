624085889e754bf57f712c54f27be1def460b35a
pavel.lisovskij <pavel.lisovskij93@gmail.com>
2020-07-01 00:43:25 +0300

    Update 1\03\task-2.php
    Решение задачи №2 по теме "Операторы циклов"

---
+++ 1/03/task-2.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="ru">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport"
+              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
+        <style>
+            body {
+                background: yellowgreen;
+            }
+        </style>
+
+        <title>Задание 2</title>
+    </head>
+    <body>
+        <?php for ($i = 0; $i <= 255; $i++): ?>
+            <p style="color: rgb(<?= $i ?>, <?= $i ?>, <?= $i ?>)">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate delectus explicabo illum iusto maiores, nobis praesentium quis quos rem suscipit! Culpa cupiditate dolores officia sit!
+            </p>
+        <?php endfor; ?>
+    </body>
+</html>
