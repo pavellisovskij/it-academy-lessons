64309906259e9765010c173cfef5612d38e5821a
pavel.lisovskij <pavel.lisovskij93@gmail.com>
2020-06-30 21:21:02 +0300

    Update 1\02\3.php
    Добавлена проверка на равенство сравниваемых чисел

--- 1/02/3.php
+++ 1/02/3.php
@@ -2,21 +2,21 @@
 
 echo '<h3>Task 3</h3>';
 
-$a = -6;
-$b = 5;
-$c = 7;
-$d = 1;
+$a = 3;
+$b = 2;
+$c = 5;
+$d = 5;
 $max = null;
 
 echo "<p>a=$a; b=$b; c=$c; d=$d.</p>";
 
-if ($a > $b && $a > $c && $a > $d) {
+if ($a >= $b && $a >= $c && $a >= $d) {
     $max = $a;
-} elseif ($b > $a && $b > $c && $b > $d) {
+} elseif ($b >= $a && $b >= $c && $b >= $d) {
     $max = $b;
-} elseif ($c > $a && $c > $b && $c > $d) {
+} elseif ($c >= $a && $c >= $b && $c >= $d) {
     $max = $c;
-} elseif ($d > $a && $d > $c && $d > $b) {
+} elseif ($d >= $a && $d >= $c && $d >= $b) {
     $max = $d;
 }
 
