e904d8b58470dcb44f028863b949605bf15caa63
pavel.lisovskij <pavel.lisovskij93@gmail.com>
2020-07-02 19:01:38 +0300

    Add tasks to №04 lesson of the first stage

---
+++ 1/04/index.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang=ru>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport"
+              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
+        <title>Решение задач по теме "Массивы"</title>
+    </head>
+    <body>
+        <div class="list-group">
+            <a href="task-1.php" class="list-group-item list-group-item-action">Задание 1</a>
+            <a href="task-2.php" class="list-group-item list-group-item-action">Задание 2</a>
+            <a href="task-3.php" class="list-group-item list-group-item-action">Задание 3</a>
+        </div>
+    </body>
+</html>
