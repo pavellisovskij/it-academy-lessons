2be72abffd6f442e0ca363f7ea96403ddbf45bcd
pavel.lisovskij <pavel.lisovskij93@gmail.com>
2020-07-28 10:07:38 +0300

    Add tasks to №09 lesson of the first stage

---
+++ 1/09/app/core.php
@@ -0,0 +1,12 @@
+<?php
+
+
+
+function debug($var) {
+    echo '<pre>';
+        var_dump($var);
+    echo '</pre>';
+    exit;
+}
+
+
---
+++ 1/09/index.php
@@ -0,0 +1,9 @@
+<?php
+
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+require 'app/core.php';
+
+
+require 'view/layout.php';
